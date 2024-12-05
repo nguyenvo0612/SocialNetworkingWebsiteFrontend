81c17aa2e786a15daa03d159a3b3e75b7ec4b5bb
ngvo.1203@gmail.com <ngvo.1203@gmail.com>
2024-12-05 20:27:52 +0700

    add create new post

--- src/app/pages/user/create-post/create-post.component.ts
+++ src/app/pages/user/create-post/create-post.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-create-post',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './create-post.component.html',
-  styleUrl: './create-post.component.css'
+  styleUrl: './create-post.component.css',
 })
 export class CreatePostComponent {
+  selectedImages: string[] = [];
 
+  onFilesSelected(event: Event) {
+    const fileInput = event.target as HTMLInputElement;
+    if (fileInput.files) {
+      this.selectedImages = Array.from(fileInput.files).map((file) =>
+        URL.createObjectURL(file)
+      );
+    }
+  }
+
+  removeImage(index: number) {
+    this.selectedImages.splice(index, 1);
+  }
 }
