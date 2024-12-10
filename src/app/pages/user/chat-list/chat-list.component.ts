a2004652896baa8b75f754d079d24e55659c16ec
ngvo.1203@gmail.com <ngvo.1203@gmail.com>
2024-12-10 10:58:16 +0700

    update

--- src/app/pages/user/chat-list/chat-list.component.ts
+++ src/app/pages/user/chat-list/chat-list.component.ts
@@ -11,9 +11,69 @@ import { Router } from '@angular/router';
 })
 export class ChatListComponent {
   chats = [
-    { user: 'Nguyen van a', message: 'Hello!', time: '10:00 AM' },
-    { user: 'Nguyen thi b', message: 'Hi there!', time: '10:01 AM' },
-    { user: 'Nguyen van v', message: 'How are you?', time: '10:02 AM' },
+    {
+      user: 'Nguyen van a',
+      message: 'Hello!',
+      time: '10:00 AM',
+      avatar:
+        'https://i.pinimg.com/564x/91/1b/88/911b881dd6f2e2e4a55838bf0072bdc1.jpg',
+    },
+    {
+      user: 'Nguyen thi b',
+      message: 'Hi there!',
+      time: '10:01 AM',
+      avatar:
+        'https://i.pinimg.com/564x/91/1b/88/911b881dd6f2e2e4a55838bf0072bdc1.jpg',
+    },
+    {
+      user: 'Nguyen van v',
+      message: 'How are you?',
+      time: '10:02 AM',
+      avatar:
+        'https://i.pinimg.com/564x/91/1b/88/911b881dd6f2e2e4a55838bf0072bdc1.jpg',
+    },
+    {
+      user: 'Nguyen van a',
+      message: 'Hello!',
+      time: '10:00 AM',
+      avatar:
+        'https://i.pinimg.com/564x/91/1b/88/911b881dd6f2e2e4a55838bf0072bdc1.jpg',
+    },
+    {
+      user: 'Nguyen thi b',
+      message: 'Hi there!',
+      time: '10:01 AM',
+      avatar:
+        'https://i.pinimg.com/564x/91/1b/88/911b881dd6f2e2e4a55838bf0072bdc1.jpg',
+    },
+    {
+      user: 'Nguyen van v',
+      message: 'How are you?',
+      time: '10:02 AM',
+      avatar:
+        'https://i.pinimg.com/564x/91/1b/88/911b881dd6f2e2e4a55838bf0072bdc1.jpg',
+    },
+    {
+      user: 'Nguyen van a',
+      message: 'Hello!',
+      time: '10:00 AM',
+      avatar:
+        'https://i.pinimg.com/564x/91/1b/88/911b881dd6f2e2e4a55838bf0072bdc1.jpg',
+    },
+    {
+      user: 'Nguyen thi b',
+      message: 'Hi there!',
+      time: '10:01 AM',
+      avatar:
+        'https://i.pinimg.com/564x/91/1b/88/911b881dd6f2e2e4a55838bf0072bdc1.jpg',
+    },
+    {
+      user: 'Nguyen van v',
+      message: 'How are you?',
+      time: '10:02 AM',
+      avatar:
+        'https://i.pinimg.com/564x/91/1b/88/911b881dd6f2e2e4a55838bf0072bdc1.jpg',
+    },
   ];
 
   constructor(private router: Router) {}
