e44a65026ee955b363ae968228d24997206244d2
nguyenvo0612 <ngvo.1203@gmail.com>
2024-12-25 15:50:10 +0700

    update

--- src/app/pages/user/message-box/message-box.component.ts
+++ src/app/pages/user/message-box/message-box.component.ts
@@ -1,128 +1,75 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../../service/auth-service';
+
+interface Message {
+  conversationId: number;
+  accountId1: number;
+  accountId2: number;
+  senderId: number;
+  content: string;
+  createdAt: string;
+}
 
 @Component({
   selector: 'app-message-box',
-  templateUrl: './message-box.component.html',
-  styleUrls: ['./message-box.component.css'],
   standalone: true,
   imports: [CommonModule, FormsModule],
+  templateUrl: './message-box.component.html',
+  styleUrls: ['./message-box.component.css']
 })
 export class MessageBoxComponent implements OnInit {
-  user: string = '';
+  messages: Message[] = [];
   newMessage: string = '';
-  messages: any[] = [];
+  conversationId: number | null = null;
+  currentAccountId: number | null = null;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
-    this.route.queryParams.subscribe((params) => {
-      this.user = params['user'];
-      this.loadFakeMessages();
+    this.currentAccountId = this.authService.getAccountId();
+    
+    this.route.queryParams.subscribe(params => {
+      this.conversationId = params['conversationId'];
+      if (this.conversationId) {
+        this.loadMessages();
+      }
     });
   }
 
-  loadFakeMessages() {
-    this.messages = [
-      {
-        conversations_id: 1,
-        user1_id: 1,
-        user2_id: 2,
-        sender_id: 1,
-        content: 'Hello!',
-        sent_at: '10:00 AM',
-      },
-      {
-        conversations_id: 1,
-        user1_id: 1,
-        user2_id: 2,
-        sender_id: 2,
-        content: 'Hi there!',
-        sent_at: '10:01 AM',
-      },
-      {
-        conversations_id: 1,
-        user1_id: 1,
-        user2_id: 2,
-        sender_id: 1,
-        content: 'How are you?',
-        sent_at: '10:02 AM',
-      },
-      {
-        conversations_id: 1,
-        user1_id: 1,
-        user2_id: 2,
-        sender_id: 2,
-        content: 'I am good, thanks! And you?',
-        sent_at: '10:03 AM',
-      },
-      {
-        conversations_id: 1,
-        user1_id: 1,
-        user2_id: 2,
-        sender_id: 1,
-        content: 'Just working on some projects.',
-        sent_at: '10:04 AM',
-      },
-      {
-        conversations_id: 1,
-        user1_id: 1,
-        user2_id: 2,
-        sender_id: 2,
-        content: 'Sounds great! Let’s catch up soon.',
-        sent_at: '10:05 AM',
-      },
-      {
-        conversations_id: 1,
-        user1_id: 1,
-        user2_id: 2,
-        sender_id: 1,
-        content: 'How are you?',
-        sent_at: '10:02 AM',
-      },
-      {
-        conversations_id: 1,
-        user1_id: 1,
-        user2_id: 2,
-        sender_id: 2,
-        content: 'I am good, thanks! And you?',
-        sent_at: '10:03 AM',
-      },
-      {
-        conversations_id: 1,
-        user1_id: 1,
-        user2_id: 2,
-        sender_id: 1,
-        content: 'Just working on some projects.',
-        sent_at: '10:04 AM',
-      },
-      {
-        conversations_id: 1,
-        user1_id: 1,
-        user2_id: 2,
-        sender_id: 2,
-        content: 'Sounds great! Let’s catch up soon.',
-        sent_at: '10:05 AM',
-      },
-    ];
+  loadMessages() {
+    if (this.conversationId) {
+      this.http.get<Message[]>(`http://localhost:8080/api/messages/messages-box/${this.conversationId}`)
+        .subscribe(data => {
+          this.messages = data;
+        });
+    }
   }
 
   sendMessage() {
-    if (this.newMessage.trim()) {
-      this.messages.push({
-        conversations_id: 1,
-        user1_id: 1,
-        user2_id: 2,
-        sender_id: 1,
+    if (this.newMessage.trim() && this.conversationId && this.currentAccountId) {
+      const message = {
+        conversationId: this.conversationId,
+        senderId: this.currentAccountId,
         content: this.newMessage,
-        sent_at: new Date().toLocaleTimeString([], {
-          hour: '2-digit',
-          minute: '2-digit',
-        }),
-      });
-      this.newMessage = '';
+      };
+
+      this.http.post<Message>('http://localhost:8080/api/messages/send', message)
+        .subscribe(response => {
+          this.messages.push(response);
+          this.newMessage = '';
+        });
     }
+  }
+
+  isCurrentUserMessage(message: Message): boolean {
+    return message.senderId === this.currentAccountId;
   }
 }
